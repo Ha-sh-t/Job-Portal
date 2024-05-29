efe50653ffb2c8b5fa3f1694d7dc0a1f120e86c6
harshit <777harshitkumar@gmail.com>
2024-05-29 18:40:21 +0530

    add Project

---
+++ controller/jobController.js
@@ -0,0 +1,28 @@
+
+
+class JobController{
+
+
+    //1.create a job
+    createJob(req , res , next){
+
+    }
+
+    //2.update a job
+    updateJob(req , res){
+
+    }
+
+    //4.delete a job
+    deleteJob(req , res){
+
+    }
+
+    //5.get job by id
+    getJobById(req ,res , next){
+
+    }
+}
+
+
+export default JobController;
---
+++ controller/userController.js
@@ -0,0 +1,22 @@
+
+
+class UserController {
+
+    //1.login
+    login(req , res ,next){
+
+    }
+
+    //2.singUp
+    register(req , res , next){
+
+    }
+
+    //3.get all users
+    getAllUsers(req , res , next){
+
+    }
+
+}
+
+export default UserController;
